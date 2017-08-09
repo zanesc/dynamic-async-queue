44b68e41309eaffb2000211fface03f9677d10b8
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-08-10 00:54:12 +0800

    Add functionality to async queue

--- index.ts
+++ index.ts
@@ -1,4 +1,36 @@
+import QueueItem from './queue-item'
+
+import { PromiseCaller } from './types'
+
 export default class AsyncQueue<T> {
-    constructor() {
+    protected waitingList: QueueItem<T>[] = []
+    protected running = 0
+
+    constructor(public concurrency: number) {
+    }
+
+    async enqueue(caller: PromiseCaller<T>) {
+        var ret = new Promise<T>((resolve, reject) => {
+            var item = new QueueItem<T>(
+                caller,
+                resolve,
+                reject,
+                () => {
+                    this.running--
+                    this.process()
+                })
+            this.waitingList.push(item)
+        })
+
+        this.process()
+
+        return ret
+    }
+
+    protected process() {
+        while (this.running < this.concurrency && this.waitingList.length > 0) {
+            this.running++
+            this.waitingList.shift().run()
+        }
     }
 }
