17ba25925f91aac0ea42d5f6806b2d513c8b7530
Zane Campbell <zanesc@me.com>
2021-01-29 16:11:12 -0400

    Added ability to remove duplicate queued items.

--- index.ts
+++ index.ts
@@ -3,34 +3,43 @@ import QueueItem from './queue-item'
 import { PromiseCaller } from './types'
 
 export default class AsyncQueue<T> {
-    protected waitingList: QueueItem<T>[] = []
-    protected running = 0
+  protected waitingList: QueueItem<T>[] = []
+  protected running = 0
 
-    constructor(public concurrency: number) {
-    }
+  constructor(public concurrency: number) {}
 
-    async enqueue(caller: PromiseCaller<T>) {
-        var ret = new Promise<T>((resolve, reject) => {
-            var item = new QueueItem<T>(
-                caller,
-                resolve,
-                reject,
-                () => {
-                    this.running--
-                    this.process()
-                })
-            this.waitingList.push(item)
-        })
-
+  async enqueue(
+    itemId: string,
+    removeDuplicateQueuedItems: boolean,
+    caller: PromiseCaller<T>,
+  ) {
+    var ret = new Promise<T>((resolve, reject) => {
+      var item = new QueueItem<T>(itemId, caller, resolve, reject, () => {
+        this.running--
         this.process()
+      })
+      this.addToWaitingList(item, removeDuplicateQueuedItems)
+      this.waitingList.push(item)
+    })
 
-        return ret
-    }
+    this.process()
 
-    protected process() {
-        while (this.running < this.concurrency && this.waitingList.length > 0) {
-            this.running++
-            this.waitingList.shift().run()
-        }
+    return ret
+  }
+
+  addToWaitingList(item: QueueItem<T>, removeDuplicateQueuedItems: boolean) {
+    if (removeDuplicateQueuedItems) {
+      this.waitingList = this.waitingList.filter(function (element) {
+        return element.getItemId() !== item.getItemId()
+      })
     }
+    this.waitingList.push(item)
+  }
+
+  protected process() {
+    while (this.running < this.concurrency && this.waitingList.length > 0) {
+      this.running++
+      this.waitingList.shift().run()
+    }
+  }
 }
--- queue-item.ts
+++ queue-item.ts
@@ -1,21 +1,28 @@
 import * as Types from './types'
 
 export default class QueueItem<T> {
-    constructor(
-        protected caller: Types.PromiseCaller<T>,
-        protected resolve: Types.ResolverCB<T>,
-        protected reject: (reason: any) => void,
-        protected complete: () => void
-    ) {
-    }
+  constructor(
+    protected itemId: string,
+    protected caller: Types.PromiseCaller<T>,
+    protected resolve: Types.ResolverCB<T>,
+    protected reject: (reason: any) => void,
+    protected complete: () => void,
+  ) {}
 
-    run() {
-        this.caller().then(val => {
-            this.resolve(val)
-        }).catch(err => {
-            this.reject(err)
-        }).then(() => {
-            this.complete()
-        })
-    }
+  run() {
+    this.caller()
+      .then((val) => {
+        this.resolve(val)
+      })
+      .catch((err) => {
+        this.reject(err)
+      })
+      .then(() => {
+        this.complete()
+      })
+  }
+
+  getItemId(): string {
+    return this.itemId
+  }
 }
